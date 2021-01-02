e5a3038860bef1b42ef358e56e2370014f7fc862
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-01-03 01:49:26 +0600

    Up to 13

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Ninjas from './Ninjas';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first reaxct app</h1>
+      <p>Welsome :) </p>
+      <Ninjas name="Ryu" age="25" belt="Black" />
+
+    </div>
+  );
+}
+
+export default App;
