7fe26cdbfd316bb073c3045072c500b0182f3e68
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-01-04 22:39:48 +0600

    17. Forms revisited

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
 const Ninjas = ({ninjas}) => {
-        const ninjaList = ninjas.map( ninja => {
-            return(
+        // const ninjaList = ninjas.map( ninja => {
+        //     if(ninja.age > 20){
+        //         return(
+        //             <div className="ninja" key={ninja.id}>
+        //                 <div>Name: { ninja.name }</div>
+        //                 <div>Age: { ninja.age }</div>
+        //                 <div>Belt: { ninja.belt }</div>
+        //             </div>
+        //         )
+        //     }else {
+        //         return null;
+        //     }
+        // });
+        const ninjaList = ninjas.map(ninja => {
+            return ninja.age > 20 ? (
                 <div className="ninja" key={ninja.id}>
                     <div>Name: { ninja.name }</div>
                     <div>Age: { ninja.age }</div>
                     <div>Belt: { ninja.belt }</div>
                 </div>
-            )
-        });
+            ) : null;
+        })
         return(
             <div className="ninja-list">
                 { ninjaList }
