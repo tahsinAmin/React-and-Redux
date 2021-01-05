83422bdf63be8ea4b7f476eafce73fb6048557f7
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-01-05 22:31:31 +0600

    19. Delete

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,37 @@
+
 import React, { Component } from 'react';
 import Ninjas from './Ninjas'
-import AddNinja from './AddNinja';
+import AddNinja from './AddNinja'
 
 class App extends Component {
-	  state = {	
+  state = {
     ninjas: [
       { name: 'Ryu', age: 30, belt: 'black', id: 1 },
       { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return id !== ninja.id;
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
-        <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja />
+        <Ninjas  deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
