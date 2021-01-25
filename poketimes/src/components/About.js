a4d7a0dd77a1d9ab07ec54f41ba5a860ac5e0fc4
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-01-26 00:50:08 +0600

    25 - The React Router

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+         <h4 className="center">About</h4>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, atque. Repellat magnam quo nobis est tenetur ad delectus tempore at, animi laboriosam dolor nulla quisquam deleniti vero cumque, voluptates excepturi.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+         <h4 className="center">Home</h4>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, atque. Repellat magnam quo nobis est tenetur ad delectus tempore at, animi laboriosam dolor nulla quisquam deleniti vero cumque, voluptates excepturi.</p>
+        </div>
+    )
+}
+
+export default Home
