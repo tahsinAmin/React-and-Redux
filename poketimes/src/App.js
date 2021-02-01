efde928d2114ba757a2ef36f0d9ee6501aef5304
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-02-01 21:37:38 +0600

    33 - Importing Images

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,26 +1,27 @@
-import './App.css';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Post from './components/Post'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-          <Switch> {/* ONly one of them shows in the browser even if the reoute matches with the other */}
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path="/:post_id" component={Post} />
-        </Switch>
-      </div>
-      
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/:post_id' component={Post} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
