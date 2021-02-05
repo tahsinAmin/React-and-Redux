15ddedec5a25b0828a0d2c4593c71458420c3154
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-02-05 16:05:19 +0600

    42 - Map Dispatch To Props

--- poketimes/src/index.js
+++ poketimes/src/index.js
@@ -13,7 +13,6 @@ const store = createStore(rootReducer);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}><App /></Provider>
-    
   </React.StrictMode>,
   document.getElementById('root')
 );
