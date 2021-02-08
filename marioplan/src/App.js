8e663b4e30a2f8b856d905f7c63c615ab83f3cc1
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-02-08 21:42:20 +0600

    #5 - Dashboard Component

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,15 +1,40 @@
-import React, { Component } from 'react'
-import { BrowserRouter } from 'react-router-dom';
-import Navbar from './layout/Navbar'
+// import React, { Component } from 'react'
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import Navbar from './layout/Navbar'
+// import Dashboard from  './components/dashboard/Dashboard'
 
-function App() {
-  return (
+// function App() {
+//   return (
+//       <BrowserRouter>
+//         <div className="App">
+//           <Navbar />
+//           <Switch> {/* Makes sure only one route is loaded at a time*/ }
+//             <Route path='/' component={Dashboard} />
+//           </Switch>
+//         </div>
+//       </BrowserRouter>
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
+
+class App extends Component {
+  render() {
+    return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path='/'component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
-  );
+    );
+  }
 }
 
 export default App;
